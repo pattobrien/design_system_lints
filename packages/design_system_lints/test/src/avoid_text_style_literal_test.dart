34cac127d21a062b122cd39df961fc3c644c7dd3
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-02-03 21:44:35 -0500

    test: GoogleFonts test.

--- packages/design_system_lints/test/src/avoid_text_style_literal_test.dart
+++ packages/design_system_lints/test/src/avoid_text_style_literal_test.dart
@@ -1,7 +1,29 @@
+import 'package:design_system_lints/design_system_lints.dart';
+import 'package:sidecar/test.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('avoid text style literal ...', () async {
-    // TODO: Implement test
+  group('description', () {
+    setUpRules([TextStyle()]);
+
+    ruleTest('GoogleFonts reference', '''
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Name extends StatelessWidget {
+  const Name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        '',
+        style: GoogleFonts.aBeeZee(),
+      ),
+    );
+  }
+}
+
+''', [ExpectedText('GoogleFonts.aBeeZee()')]);
   });
 }
